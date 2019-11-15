22099d0475143bc6273b5b0e8d7772aa72176c3a
Trai Nguyen <tnguyen@at-pac.com>
2019-11-15 10:41:32 -0500

    added functions to match with existing user

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,7 +6,40 @@
 // ===============================================================================
 
 var friendArray = [
-    
+    {
+        "name": "goku",
+        "photo": "https://comicvine1.cbsistatic.com/uploads/original/11137/111372607/6749946-46108866_2138130396245355_1197692474502530083_n.jpg",
+        "scores": [
+        "2",
+        "2",
+        "3",
+        "4",
+        "4",
+        "4",
+        "4",
+        "3",
+        "3",
+        "3"
+        ],
+        "totalScore": "32"
+    },
+    {
+        "name": "gohan",
+        "photo": "https://comicvine1.cbsistatic.com/uploads/original/11137/111372607/6749946-46108866_2138130396245355_1197692474502530083_n.jpg",
+        "scores": [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "4"
+        ],
+        "totalScore": "10"
+    }
   ];
   
   // Note how we export the array. This makes it accessible to other files using require.
